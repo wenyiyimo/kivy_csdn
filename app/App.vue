ebdd128d615d339b0552e972d68182c143b8058b
wenyiyimo <81872056+wenyiyimo@users.noreply.github.com>
2022-05-27 09:51:48 +0800

    Update App.vue

--- app/App.vue
+++ app/App.vue
@@ -1,7 +1,18 @@
 <script>
+import db from './utils/database.js';
 export default {
 	onLaunch: function() {
-		console.log('App Launch');
+		db.initdb();
+		uni.setKeepScreenOn({
+			keepScreenOn: true
+		});
+		let sWidth = uni.getSystemInfoSync().screenWidth;
+		let sHeight = uni.getSystemInfoSync().screenHeight;
+		if (sWidth > sHeight) {
+			plus.screen.lockOrientation('landscape');
+		} else {
+			plus.screen.lockOrientation('portrait');
+		}
 	},
 	onShow: function() {
 		console.log('App Show');
@@ -15,4 +26,28 @@ export default {
 <style lang="scss">
 /*每个页面公共css */
 @import url('@/common/common.scss');
+
+/* #ifdef MP-WEIXIN || APP-PLUS */
+::-webkit-scrollbar {
+	display: none;
+	width: 0 !important;
+	height: 0 !important;
+	-webkit-appearance: none;
+	background: transparent;
+	color: transparent;
+}
+/* #endif */
+
+/* 解决H5 的问题 */
+/* #ifdef H5 */
+uni-scroll-view .uni-scroll-view::-webkit-scrollbar {
+	/* 隐藏滚动条，但依旧具备可以滚动的功能 */
+	display: none;
+	width: 0 !important;
+	height: 0 !important;
+	-webkit-appearance: none;
+	background: transparent;
+	color: transparent;
+}
+/* #endif */
 </style>
