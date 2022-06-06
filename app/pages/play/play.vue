f33568b934e8f2057a91829c6d26bb44c4a611a7
wenyiyimo <81872056+wenyiyimo@users.noreply.github.com>
2022-06-06 18:02:12 +0800

    Create play.vue

---
+++ app/pages/play/play.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="play">
+		<view class="header">
+			<view class="header-top"></view>
+			<view style="display: flex;flex-direction: row;justify-content: space-between;">
+				<uni-icons class="icon-style" color="#000000" size="40" type="arrow-left" @click="pageBacked" />
+				<text class="first-text" style="margin-top: 5rpx;" @click="openImpSite()">播放</text>
+				<uni-icons style="margin-right: 10px;" color="#000000" size="40" :type="downImage" @click="downvideo" />
+			</view>
+			<scroll-view style="white-space: nowrap" scroll-x="true" show-scrollbar="false" scroll-left="120">
+				<view style="display: flex;flex-direction: row;justify-content: flex-start;margin-top: 5rpx;margin-bottom: 15rpx;">
+					<view v-for="(item, index) in searchlists" :key="index">
+						<view class="first-text" style="background-color: #ebebeb;border-radius: 20%;margin-right: 10rpx;">{{ item[0].state }}</view>
+					</view>
+				</view>
+			</scroll-view>
+			<scroll-view style="white-space: nowrap" scroll-x="true" show-scrollbar="false" scroll-left="120">
+				<view style="display: flex;flex-direction: row;justify-content: flex-start;margin-top: 5rpx;margin-bottom: 15rpx;">
+					<view class="first-text" @click="nixu">逆序</view>
+					<view v-for="(item, index) in playdatas" :key="index">
+						<view class="first-text" style="background-color: #ebebeb;border-radius: 20%;margin-right: 10rpx;">{{ item.name }}</view>
+					</view>
+				</view>
+			</scroll-view>
+		</view>
+		<view class="body"></view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			downImage: 'arrow-down',
+			sitecurrent: 0,
+			tagcurrent: 0,
+			playcurrent: 0,
+			searchlists: [],
+			playdatas: []
+		};
+	},
+	methods: {
+		pageBacked() {
+			// this.$router.go(-1);
+			uni.navigateBack();
+		},
+		nixu() {
+			let temp = this.playdatas[this.tagcurrent].data;
+			let tempLen = temp.length;
+			for (let i = 0; i < tempLen; i++) {
+				this.playdatas[this.tagcurrent]['data'][i] = temp[tempLen - i - 1];
+			}
+		}
+	},
+	onLoad: function(option) {
+		this.sitecurrent = Number(option.index);
+		this.searchlists = uni.getStorageSync('temp');
+	}
+};
+</script>
+
+<style></style>
