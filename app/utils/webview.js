e2cec8c04eb174379a00bd3b036323d129457846
wenyiyimo <81872056+wenyiyimo@users.noreply.github.com>
2022-06-10 14:52:07 +0800

    Update webview.js

--- app/utils/webview.js
+++ app/utils/webview.js
@@ -7,9 +7,9 @@ export default {
             videoContext: null,
         };
     },
-    
-  //  import webvplay from '@/utils/webview.js';
-//export default {
+
+    //  import webvplay from '@/utils/webview.js';
+    //export default {
     //mixins: [webvplay],
     methods: {
         async addvideo() {
@@ -25,100 +25,77 @@ export default {
                 httpCache: true,
                 playStrategy: 3
             });
+            let currentWebview = this.$scope.$getAppWebview();
+            currentWebview.append(this.videoContext);
+            this.videoContext.hide();
         },
-        xtcallback(e) {
-			console.log(e.url);
-            if (
-                e.url.indexOf('?url=') == -1 &&
-                e.url.indexOf('beiyong') == -1 &&
-                e.url.indexOf('SVG') == -1 &&
-                e.url.indexOf('update') == -1 &&
-                e.url.indexOf('51.la/') == -1 &&
-                !e.url.endsWith('.js') &&
-                e.url.indexOf('.png') == -1 &&
-                e.url.indexOf('.css') == -1 &&
-                e.url.indexOf('gif') == -1 &&
-                e.url.indexOf('.php') == -1 &&
-                !e.url.endsWith('.jpg') &&
-                e.url.indexOf('umuuid') == -1
-            ) {
-                let matchrule = uni.getStorageSync('matchrule');
-                let re = new RegExp(matchrule, 'g')
-                let t = re.exec(e.url);
-                if (!t) {
-                    this.playurl = e.url;
-                    console.log(this.playurl);
-                }
-            }
-        },
-         getplayurl(url, matchrule = '!!!') {
+        xtplayurl(url, matchrule = '!!!') {
             uni.setStorageSync('matchrule', matchrule);
-            // uni.showToast({
-            //     title: '嗅探中...',
-            //     duration: 30000,
-            //     icon: 'loading'
-            // });
-            this.webv.clear(); //清除之前的加载
+            uni.showToast({
+                title: '嗅探中...',
+                duration: 30000,
+                icon: 'loading'
+            });
             uni.setStorageSync('playurl', '');
             this.webv.loadURL(url);
-			console.log(url)
+            console.log(url)
             // this.webv.overrideUrlLoading({ mode: 'reject' }, function(e) {});
+            this.webv.overrideResourceRequest([{ match: '.*jpg|.*js|.*png|.*css|.*gif|.*php|.*SVG', redirect: '' }])
             this.webv.overrideUrlLoading({
                 mode: 'allow',
                 match: '.*(mp4|m3u8|flv|url=|video).*'
             }, function(e) {
-				                    // console.log(e.url);
-			});
+                // console.log(e.url);
+            });
             this.webv.listenResourceLoading({
                 match: '.*(mp4|m3u8|flv|url=|video).*'
-            }, function(e){
-				if (
-				    e.url.indexOf('?url=') == -1 &&
-				    e.url.indexOf('beiyong') == -1 &&
-				    e.url.indexOf('SVG') == -1 &&
-				    e.url.indexOf('update') == -1 &&
-				    e.url.indexOf('51.la/') == -1 &&
-				    !e.url.endsWith('.js') &&
-				    e.url.indexOf('.png') == -1 &&
-				    e.url.indexOf('.css') == -1 &&
-				    e.url.indexOf('gif') == -1 &&
-				    e.url.indexOf('.php') == -1 &&
-				    !e.url.endsWith('.jpg') &&
-				    e.url.indexOf('umuuid') == -1
-				) {
-				    let matchrule = uni.getStorageSync('matchrule');
-				    let re = new RegExp(matchrule, 'g')
-				    let t = re.exec(e.url);
-				    if (!t) {
-						console.log(1111111)
-						console.log(e.url)
-						console.log(this)
-				        this.playurl = e.url;
-				        console.log(this.playurl);
-				    }
-				}
-			})
-            // let that = this;
-            // let alltime = 0;
-            // let obj = setInterval(function() {
-            //     let urlPlay = uni.getStorageSync('playurl');
-            //     alltime = alltime + 2000;
-            //     if (alltime < 20000 && urlPlay != '') {
-            //         uni.hideToast();
-            //         that.playurl = urlPlay;
-            //         that.webv.clear();
-            //         clearInterval(obj);
-            //     }
-            //     if (alltime > 20000) {
-            //         uni.hideToast();
-            //         uni.showToast({
-            //             title: '嗅探超时！',
-            //             duration: 1000
-            //         });
-            //         that.webv.clear();
-            //         clearInterval(obj);
-            //     }
-            // }, 2000);
+            }, function(e) {
+                if (
+                    e.url.indexOf('?url=') == -1 &&
+                    e.url.indexOf('beiyong') == -1 &&
+                    e.url.indexOf('SVG') == -1 &&
+                    e.url.indexOf('update') == -1 &&
+                    e.url.indexOf('51.la/') == -1 &&
+                    !e.url.endsWith('.js') &&
+                    e.url.indexOf('.png') == -1 &&
+                    e.url.indexOf('.css') == -1 &&
+                    e.url.indexOf('gif') == -1 &&
+                    e.url.indexOf('.php') == -1 &&
+                    !e.url.endsWith('.jpg') &&
+                    e.url.indexOf('umuuid') == -1
+                ) {
+                    let matchrule = uni.getStorageSync('matchrule');
+                    let re = new RegExp(matchrule, 'g')
+                    let t = re.exec(e.url);
+                    if (!t) {
+                        console.log(e.url);
+                        uni.setStorageSync('playurl', e.url);
+                    }
+                }
+            })
+            let that = this;
+            let alltime = 0;
+            let obj = setInterval(function() {
+                let urlPlay = uni.getStorageSync('playurl');
+                alltime = alltime + 2000;
+                if (alltime < 20000 && urlPlay != '') {
+                    uni.hideToast();
+                    that.playurl = urlPlay;
+                    that.webv.loadURL('');
+                    that.webv.clear();
+                    clearInterval(obj);
+                }
+                if (alltime > 20000) {
+                    uni.hideToast();
+                    uni.showToast({
+                        title: '嗅探超时！',
+                        duration: 1000
+                    });
+                    that.webv.loadURL('');
+                    that.webv.clear();
+                    clearInterval(obj);
+                }
+            }, 2000);
         },
         preweb() {
             this.webv = plus.webview.create('', 'xtwebview', {
@@ -135,6 +112,7 @@ export default {
 
     onLoad: function() {
         this.preweb();
+        this.addvideo();
     },
     onUnload: function() {
         this.webv.close();
